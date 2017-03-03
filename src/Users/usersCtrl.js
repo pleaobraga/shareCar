01d81a749ae3d14bf73c10bf74bb1eba4c7f2592
pleaobraga <pleao.braga@gmail.com>
2017-03-03 19:00:45 -0300

    tela usuários toda construida

--- src/Users/usersCtrl.js
+++ src/Users/usersCtrl.js
@@ -3,11 +3,16 @@ shareCar.controller("usersCtrl", ['$scope', function($scope){
   $scope.users = [1,2,3];
   $scope.showOptionIcons = [];
   $scope.classImag = [];
+  $scope.classIcon = [];
   $scope.usersActivate = [{id: 0, name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
   phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true }, {name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
   phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true },{name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
   phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true },{name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
   phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true }];
+
+  $scope.usersInvited = [{id: 0, name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com' },
+    {id: 0, name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com' },
+  {id: 0, name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com' }];
 
   //$scope.usersActivate = [];
 
@@ -19,12 +24,24 @@ shareCar.controller("usersCtrl", ['$scope', function($scope){
   $scope.showOptionIcons = function(index)
   {
     $scope.showOptionIcons[index] = true;
-    $scope.classImag[index] = {'margin-top': '-68px'};
+    $scope.classIcon[index] = {'margin-top': '-90px'};
   };
 
   $scope.hideOptionIcons = function(index)
   {
     $scope.showOptionIcons[index] = false;
+    $scope.classIcon[index] = {'margin-top': '16px'};
+  };
+
+  $scope.showOptionImage = function(index)
+  {
+    $scope.showOptionImage[index] = true;
+    $scope.classImag[index] = {'margin-top': '-68px'};
+  };
+
+  $scope.hideOptionImage = function(index)
+  {
+    $scope.showOptionImage[index] = false;
     $scope.classImag[index] = {'margin-top': '16px'};
   };
 
@@ -38,7 +55,7 @@ shareCar.controller("usersCtrl", ['$scope', function($scope){
     //usersService
     //edit function
   };
-  
+
   $scope.deleteUser = function(id)
   {
     //usersService
