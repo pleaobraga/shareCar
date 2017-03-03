4708988752857f44ef0d0db238c13d427d33de1a
pleaobraga <pleao.braga@gmail.com>
2017-03-02 22:56:07 -0300

    card usuário ativo funcionando e criado automaticamente

--- src/Users/usersCtrl.js
+++ src/Users/usersCtrl.js
@@ -1,10 +1,47 @@
 shareCar.controller("usersCtrl", ['$scope', function($scope){
 
   $scope.users = [1,2,3];
+  $scope.showOptionIcons = [];
+  $scope.classImag = [];
+  $scope.usersActivate = [{id: 0, name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
+  phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true }, {name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
+  phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true },{name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
+  phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true },{name: 'Antonio Leão Braga', email: 'pedro_leao_braga@hotmail.com',
+  phone: '(31)99993-3774', car_name: 'Picasso', maker: 'Land Rover', owner: true }];
+
+  //$scope.usersActivate = [];
 
   $scope.invite = function()
   {
     //userService
-  }
+  };
+
+  $scope.showOptionIcons = function(index)
+  {
+    $scope.showOptionIcons[index] = true;
+    $scope.classImag[index] = {'margin-top': '-68px'};
+  };
+
+  $scope.hideOptionIcons = function(index)
+  {
+    $scope.showOptionIcons[index] = false;
+    $scope.classImag[index] = {'margin-top': '16px'};
+  };
+
+  $scope.associateAnotherVehicle = function(idUser)
+  {
+    //veicleRedict
+  };
+
+  $scope.edit = function(id)
+  {
+    //usersService
+    //edit function
+  };
+  
+  $scope.deleteUser = function(id)
+  {
+    //usersService
+  };
 
 }]);
